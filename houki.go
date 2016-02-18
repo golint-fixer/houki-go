29572d9411fb920df03fabea1c439686e120a1c5
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2016-02-19 08:07:44 +0900

    use data that is specified in the config

--- houki.go
+++ houki.go
@@ -7,37 +7,39 @@ import (
 	"os"
 )
 
+type Config struct {
+	Directory []string `yaml:"Directory"`
+}
+
 func removeDirectories(directories []string) {
 	for _, directory := range directories {
 
 		if err := os.RemoveAll(directory); err != nil {
 			fmt.Println(err)
 		}
+		fmt.Printf("Remove: %s\n", directory)
 	}
 }
 
-func readConfigFile() {
+func readConfigFile() Config {
 	configFile := os.Getenv("HOME") + "/.houki.yml"
 
 	buf, err := ioutil.ReadFile(configFile)
 	if err != nil {
-		fmt.Println(err)
-		return
-	}
-
-	m := make(map[interface{}]interface{})
-	err = yaml.Unmarshal(buf, &m)
-	if err != nil {
 		panic(err)
 	}
 
-	fmt.Printf("%s\n", m["Directory"])
+	var parsedMap Config
+	if err = yaml.Unmarshal(buf, &parsedMap); err != nil {
+		panic(err)
+	}
+
+	return parsedMap
 }
 
 func main() {
-	readConfigFile()
-	directories := []string{"tmp"}
-	removeDirectories(directories)
+	config := readConfigFile()
+	removeDirectories(config.Directory)
 
 	fmt.Println("Have cleaned")
 }
